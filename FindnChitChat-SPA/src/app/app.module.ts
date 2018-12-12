06047564dec072c52b797ae1b89683669d11d2c8
mahesh <maheshvallikunnu@gmail.com>
2018-12-12 14:35:25 +0530

    guard

--- FindnChitChat-SPA/src/app/app.module.ts
+++ FindnChitChat-SPA/src/app/app.module.ts
@@ -17,6 +17,7 @@ import { MemberListComponent } from './member-list/member-list.component';
 import { MessagesComponent } from './messages/messages.component';
 import { ListsComponent } from './lists/lists.component';
 import { appRoutes } from './routes';
+import { AuthGuard } from './_guards/auth.guard';
 
 
 @NgModule({
@@ -40,7 +41,8 @@ import { appRoutes } from './routes';
    providers: [
       AuthService,
       ErrorInterceptorProvider,
-      AlertifyService
+      AlertifyService,
+      AuthGuard
    ],
    bootstrap: [
       AppComponent
