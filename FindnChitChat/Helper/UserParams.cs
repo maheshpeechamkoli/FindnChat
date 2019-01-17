32276bbe6b818778198d1837fc35e2aad9e10305
maheshpeechamkoli <maheshvallikunnu@gmail.com>
2019-01-17 16:05:15 +0530

    Sorting Fileters

--- FindnChitChat/Helper/UserParams.cs
+++ FindnChitChat/Helper/UserParams.cs
@@ -17,6 +17,7 @@ namespace FindnChitChat.Helper
         public string Gender { get; set; }
         public int MinAge { get; set; } = 18;
         public int MaxAge { get; set; } = 99;
+        public string OrderBy { get; set; }
 
     }
 }
